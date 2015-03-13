b6747654d9dfe8f3185bdf7124fedae09d6bbbd0
Arkadiusz Stépniewski <a.stepniewski@hotmail.com>
2015-03-13 09:45:33 +0100

    rozdzielilem testy dla srodkowego elementu sekwencji uwzgledniajac czy sekwencja jest parzysta czy nie

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -13,69 +13,74 @@ import org.junit.Test;
 public class BinarySearchTest {
 
 	private static BinarySearch binarySearch;
-	 
-    @BeforeClass
-    public static void oneTimeSetUp() {
-        binarySearch = new BinarySearch();
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+	@BeforeClass
+	public static void oneTimeSetUp() {
+		binarySearch = new BinarySearch();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testEmptySeq() {
 		int[] seq = new int[0];
 		binarySearch.search(1, seq);
 	}
-    
-    @Test
-    public void testKeyIsInSeqLenght1(){
-    	int[] seq = new int[1];
-    	int key = 22;
-    	seq[0] = key;
-    	SearchResult searchResult = binarySearch.search(key, seq);
-    	assertTrue(searchResult.isFound());
-    }
-    
-    @Test
-    public void testKeyIsNotInSeqLenght1(){
-    	int[] seq = new int[1];
-    	int key = 22;
-    	seq[0] = 12;
-    	SearchResult searchResult = binarySearch.search(key, seq);
-    	assertFalse(searchResult.isFound());
-    }
-    
-    @Test
-    public void testKeyIsFirstElementInSeq(){
-    	int[] seq = {1,3,6,8,9};
-    	int key = 1;
-    	SearchResult searchResult = binarySearch.search(key, seq);
-    	assertEquals(1, searchResult.getPosition());
-    }
-    
-    @Test
-    public void testKeyIsLastElementInSeq(){
-    	int[] seq = {1,3,6,8,9};
-    	int key = 9;
-    	SearchResult searchResult = binarySearch.search(key, seq);
-    	assertEquals(seq.length, searchResult.getPosition());
-    }
-    
-    @Test
-    public void testKeyIsInTheMiddleOfSeq(){
-    	int[] seq = {1,3,6,8,9,11};
-    	int key = 6;
-    	SearchResult searchResult = binarySearch.search(key, seq);
-    	assertEquals(seq.length/2, searchResult.getPosition());
-    }
-    
-    @Test
-    public void testKeyIsNotInSeqLenghtGreaterThan1(){
-    	int[] seq = {1,3,6,8,9};
-    	int key = 99;
-    	SearchResult searchResult = binarySearch.search(key, seq);
-    	assertFalse(searchResult.isFound());
-    }
-    
-    
 
+	@Test
+	public void testKeyIsInSeqLenght1() {
+		int[] seq = new int[1];
+		int key = 22;
+		seq[0] = key;
+		SearchResult searchResult = binarySearch.search(key, seq);
+		assertTrue(searchResult.isFound());
+	}
+
+	@Test
+	public void testKeyIsNotInSeqLenght1() {
+		int[] seq = new int[1];
+		int key = 22;
+		seq[0] = 12;
+		SearchResult searchResult = binarySearch.search(key, seq);
+		assertFalse(searchResult.isFound());
+	}
+
+	@Test
+	public void testKeyIsFirstElementInSeq() {
+		int[] seq = { 1, 3, 6, 8, 9 };
+		int key = 1;
+		SearchResult searchResult = binarySearch.search(key, seq);
+		assertEquals(1, searchResult.getPosition());
+	}
+
+	@Test
+	public void testKeyIsLastElementInSeq() {
+		int[] seq = { 1, 3, 6, 8, 9 };
+		int key = 9;
+		SearchResult searchResult = binarySearch.search(key, seq);
+		assertEquals(seq.length, searchResult.getPosition());
+	}
+
+	@Test
+	public void testKeyIsInTheMiddleOfEvenSeq() {
+		int[] seq = { 1, 3, 6, 8, 9, 11 };
+		int key = 6;
+		SearchResult searchResult = binarySearch.search(key, seq);
+		assertEquals(seq.length / 2, searchResult.getPosition());
+	}
+
+	@Test
+	public void testKeyIsInTheMiddleOfOddSeq() {
+		int[] seq = { 1, 3, 6, 8, 9 };
+		int key = 6;
+		SearchResult searchResult = binarySearch.search(key, seq);
+		assertEquals(seq.length / 2 + 1, searchResult.getPosition());
+	}
+
+	@Test
+	public void testKeyIsNotInSeqLenghtGreaterThan1() {
+		int[] seq = { 1, 3, 6, 8, 9 };
+		int key = 99;
+		SearchResult searchResult = binarySearch.search(key, seq);
+		assertFalse(searchResult.isFound());
+	}
 
 }
